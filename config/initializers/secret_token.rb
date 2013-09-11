77c482f470180db2e8bff72f3f14f7cedc67c9f8
kayakyakr <kayakyakr@gmail.com>
2013-09-10 21:19:59 -0500

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SumTimes::Application.config.secret_token = '67b29127992b47c79fc8e8db4b50c1fcdae092c3d40d0b58b14e6c879a258d44df587272fe19b0e6e977f756aa3be756ff17b80d7bac28f41d1d235ed111b13a'
+SumTimes::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
